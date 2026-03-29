26ea6002742e9a60f58dcfdb8b808dbd37665660
Yasaswini-max <yasaswiniraomedikonda@gmail.com>
2026-03-29 19:21:21 +0530

    Update model.py

--- model.py
+++ model.py
@@ -2,7 +2,7 @@ from sentence_transformers import SentenceTransformer
 import numpy as np
 import faiss
 
-model = None  # don't load immediately
+model = None
 
 def get_model():
     global model
@@ -10,8 +10,6 @@ def get_model():
         model = SentenceTransformer('all-MiniLM-L6-v2')
     return model
 
-
-# Create index
 def create_index(data):
     model = get_model()
     embeddings = model.encode(data)
@@ -19,8 +17,6 @@ def create_index(data):
     index.add(np.array(embeddings))
     return index
 
-
-# Search function
 def search(query, data, index):
     model = get_model()
     q_emb = model.encode([query])
