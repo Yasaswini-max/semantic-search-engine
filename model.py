2e35cbe3b692147285575a4d99962fef897a75d4
Yasaswini-max <yasaswiniraomedikonda@gmail.com>
2026-03-29 18:46:18 +0530

    Update model.py

--- model.py
+++ model.py
@@ -2,18 +2,27 @@ from sentence_transformers import SentenceTransformer
 import numpy as np
 import faiss
 
-# Load model (this converts text → numbers)
-model = SentenceTransformer('all-MiniLM-L6-v2')
+model = None  # don't load immediately
 
-# Create index (store all data)
+def get_model():
+    global model
+    if model is None:
+        model = SentenceTransformer('all-MiniLM-L6-v2')
+    return model
+
+
+# Create index
 def create_index(data):
+    model = get_model()
     embeddings = model.encode(data)
     index = faiss.IndexFlatL2(embeddings.shape[1])
     index.add(np.array(embeddings))
     return index
 
+
 # Search function
 def search(query, data, index):
+    model = get_model()
     q_emb = model.encode([query])
     D, I = index.search(q_emb, k=1)
     return data[I[0][0]]
